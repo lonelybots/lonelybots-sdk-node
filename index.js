b55a862e31e1a53a094db373438bfd060ca8ee7d
Kun Chen <kunchenguid@gmail.com>
2016-05-06 21:57:54 -0700

    add support for creating forms and replying

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var botMiddleware = require('./lib/bot-middleware')
+var getAttachment = require('./lib/get-attachment')
 var sendEmail = require('./lib/send-email')
+var reply = require('./lib/reply')
+var replyAll = require('./lib/reply-all')
+var createForm = require('./lib/create-form')
 var ArgumentException = require('./lib/exceptions').ArgumentException
 
 module.exports = function (options) {
@@ -7,14 +11,20 @@ module.exports = function (options) {
     throw new ArgumentException('options', 'missing required arguments')
   }
   if (!options.validationToken) {
-    throw new ArgumentException('options.validationToken', 'missing required arguments')
+    throw new ArgumentException(
+      'options.validationToken', 'missing required arguments')
   }
   if (!options.accessToken) {
-    throw new ArgumentException('options.accessToken', 'missing required arguments')
+    throw new ArgumentException(
+      'options.accessToken', 'missing required arguments')
   }
 
   return {
     botMiddleware: botMiddleware(options),
-    sendEmail: sendEmail(options)
+    getAttachment: getAttachment(options),
+    sendEmail: sendEmail(options),
+    reply: reply(options),
+    replyAll: replyAll(options),
+    createForm: createForm(options)
   }
 }
